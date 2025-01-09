250133c447dc7f6dbcd99150e268fd4cbdc9dedb
Yokaish <kauaoficialkr@outlook.com>
2025-01-08 21:44:22 -0300

    header and main section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        plusJakarta: ['Plus Jakarta Sans', 'serif'],
+      },
+      colors: {
+        mainWhite: '#F9F9F9',
+        grayBg: '#F0F0F0',
+        textColor: '#222831',
+        secondaryTextColor: '#878C91',
+        primaryRed: '#FF204E',
+        secondaryRed: '#94122D',
+        accentRed: '#FF4F73'
+      },
+      backgroundImage: {
+        main: "url('/src/assets/img/main-bg.jpg')",
+        driveImg: "url('/src/assets/img/drive-graphic.jpg')"
+      },
+    },
   },
   plugins: [],
 }
