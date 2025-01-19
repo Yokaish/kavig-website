69e8bfd29645668b0e23cc8bcf02700b8398590f
Yokaish <kauaoficialkr@outlook.com>
2025-01-19 15:58:35 -0300

    started hamburguer menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,5 +18,17 @@ document.querySelectorAll('.plus-btn, .minus-btn').forEach(button => {
       }
     });
   });
+
+  function toggleMenu() {
+    let menu = document.getElementById('mobile-menu');
+    let icon = document.getElementById('menu-icon');
+    if (menu.classList.contains('open')) {
+        menu.classList.remove('open');
+        icon.src = "icons/hamb-menu.svg";
+    } else {
+        menu.classList.add('open');
+        icon.src = "icons/hamb-menu-closed.svg";
+    }
+}
   
   
